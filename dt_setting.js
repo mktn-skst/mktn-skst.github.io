c701139c06af376eab9092cdcac3736b67b11d52
mktn-skst <skst.mktn@gmail.com>
2016-11-09 19:12:59 +0900

    Add files via upload

---
+++ dt_setting.js
@@ -0,0 +1,18 @@
+jQuery(function($){
+	$.extend( $.fn.dataTable.defaults, {
+		language: { url: "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json" }
+	});
+
+	$("#chara-table").dataTable({
+        // 件数切替機能 無効
+        lengthChange: false,
+        // 検索機能 有効
+        searching: true,
+        // ソート機能 有効
+        ordering: true,
+        // 情報表示 無効
+        info: false,
+        // ページング機能 無効
+        paging: false
+    });
+});
