6dc08c53cdb9205dbca2efe77f2e7800b78a5298
mktn-skst <skst.mktn@gmail.com>
2016-11-09 22:27:11 +0900

    Update dt_setting.js

--- dt_setting.js
+++ dt_setting.js
@@ -1,6 +1,6 @@
 jQuery(function($){
 	$.extend( $.fn.dataTable.defaults, {
-		language: { url: "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json" }
+		/*language: { url: "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Japanese.json" }*/
 	});
 
 	$("#chara-table").dataTable({
