4966d8cd3299b05bc4d9798b97f5fc1100401546
mktn-skst <skst.mktn@gmail.com>
2016-11-10 23:57:51 +0900

    Update dt_setting.js

--- dt_setting.js
+++ dt_setting.js
@@ -4,15 +4,20 @@ jQuery(function($){
 	});
 
 	$("#chara-table").dataTable({
-        // 件数切替機能 無効
-        lengthChange: false,
-        // 検索機能 有効
-        searching: true,
-        // ソート機能 有効
-        ordering: true,
-        // 情報表示 無効
-        info: false,
-        // ページング機能 無効
-        paging: false
-    });
+		// 件数切替機能 無効
+		lengthChange: false,
+		// 検索機能 有効
+		searching: true,
+		// ソート機能 有効
+		ordering: true,
+		// 情報表示 無効
+		info: false,
+		// ページング機能 無効
+		paging: false
+		// 横スクロールバーを有効にする (scrollXはtrueかfalseで有効無効を切り替えます)
+		scrollX: true,	
+		// 縦スクロールバーを有効にする (scrollYは200, "200px"など「最大の高さ」を指定します)
+		scrollY: 200
+    
+	});
 });
